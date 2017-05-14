0e5d2ab1f560a79c0d9117fbf1a06c0cc7659d85
Aniluj <julianvolonterio@gmail.com>
2017-05-14 18:01:18 -0300

    Commit-Proyecto Terminado
    Primer commit del sistema solar en su versión final.

---
+++ Sistema Solar/Assets/Rotacion.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotacion : MonoBehaviour {
+
+	public float ejeDeRotacion;
+		
+	// Update is called once per frame
+	void Update () {
+		
+		transform.Rotate (new Vector3 (0, ejeDeRotacion,0));
+	}
+}
---
+++ Sistema Solar/Assets/Traslacion.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Traslacion : MonoBehaviour {
+
+	public Transform posicionDelObjetoARotar;
+	public float velocidad;
+
+	// Update is called once per frame
+	void Update () {
+
+		transform.RotateAround (posicionDelObjetoARotar.position, Vector3.up, velocidad);
+	}
+}
